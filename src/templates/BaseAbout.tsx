34af33268ed7abece7874880b4dfd9a9c9c68e20
macallegro <163568164+macallegro@users.noreply.github.com>
2024-03-15 22:04:08 +0000

    3

--- src/templates/BaseAbout.tsx
+++ src/templates/BaseAbout.tsx
@@ -2,15 +2,15 @@ import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
 import { Footer } from './Footer';
 import { HeroKontakt } from './HeroKontakt';
-import { VerticalFeaturesKontakt } from './VerticalFeaturesKontakt';
+import { VerticalFeaturesAbout } from './VerticalFeaturesAbout';
 
-const BaseKontakt = () => (
+const BaseAbout = () => (
   <div className="text-gray-600 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <HeroKontakt />
-    <VerticalFeaturesKontakt />
+    <VerticalFeaturesAbout />
     <Footer />
   </div>
 );
 
-export { BaseKontakt };
+export { BaseAbout };
